3e517b58aaa709a62de58f38d5b155152cde20eb
Ïgor Zakharov <Igor1994294@yandex.ru>
2020-08-01 17:33:43 +0300

    fix: исправить ошибки в javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,34 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let exitButton = document.querySelector('.popup__button-reset');
-let formElement = document.querySelector('.popup__form');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let nameInput = formElement.querySelector('.popup__input:first-of-type');
-let jobInput = formElement.querySelector('.popup__input:last-of-type');
+let popup = document.querySelector('.popup'); //Окно с popup
+let editButton = document.querySelector('.profile__edit-button'); //Кнопка редактирования
+let exitButton = document.querySelector('.popup__button-reset'); //Кнопка выхода
+let formElement = document.querySelector('.popup__form');  //Форма
+let profileTitle = document.querySelector('.profile__title'); //Имя на странице в секции профиль
+let profileSubtitle = document.querySelector('.profile__subtitle'); //Должность на странице в секции профиль
+let nameInput = formElement.querySelector('.popup__input:first-of-type'); //Поле ввода имени
+let jobInput = formElement.querySelector('.popup__input:last-of-type'); //Поле ввода должности
 
-
-editButton.addEventListener('click', openEdit);
-exitButton.addEventListener('click', exitEdit);
-
-
-function openEdit(){
-	popup.classList.add('popup_opened');
-	nameInput.value = profileTitle.textContent;
-	jobInput.value = profileSubtitle.textContent;
+//Функция для открытия/закрытия модального окна, через класс popup_opened
+function controlEdit(){
+  if(popup.classList.contains('popup_opened') == true){
+    popup.classList.remove('popup_opened');
+  } else{
+    popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+  }
 }
-
-
-function exitEdit(){
-	popup.classList.remove('popup_opened');
-}
-
 
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                         // Так мы можем определить свою логику отправки.
                         // О том, как это делать, расскажем позже.
-    // Воспользуйтесь инструментом .querySelector()
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value;
-    profileTitle.textContent = nameValue;
-    profileSubtitle.textContent = jobValue;
-    popup.classList.remove('popup_opened');
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+  profileTitle.textContent = nameValue;  //Сверяю значения на странице с полем ввода для имени
+  profileSubtitle.textContent = jobValue; //Сверяю значения на странице с полем ввода для должности
+  controlEdit();  //Закрываю окно
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', controlEdit);  //Вход в окно редактирования 
+exitButton.addEventListener('click', controlEdit);  //Выход из окна редактирования
