8f763a546a8ba58e4e216c20302587792ab307b0
Ïgor Zakharov <Igor1994294@yandex.ru>
2020-10-03 14:40:48 +0300

    hotfix: исправить расположение переменных, валидацию

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,24 @@
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
-import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import Section from '../components/Section.js';
-import {initialCards} from '../utils/constants.js';
+import {
+  initialCards, 
+  modalWindowProfile, 
+  modalWindowCard,
+  modalWindowImage,
+  profileButton,
+  profileName,
+  profileJob,
+  formProfile,
+  nameInput,
+  jobInput,
+  cardsButton,
+  elements,
+  formCard
+} from '../utils/constants.js';
 import UserInfo from '../components/UserInfo.js';
-
-const popups = document.querySelectorAll('.popup');
-const modalWindowProfile = document.querySelector('.popup:nth-of-type(1n)'); //Окно с popup редактирования профиля
-const modalWindowCard = document.querySelector('.popup:nth-of-type(2n)'); //Окно с popup редактирования места
-const modalWindowImage = document.querySelector('.popup:nth-of-type(3n)'); //Окно с popup изображением
-const profileButton = document.querySelector('.profile__edit-button'); //Кнопка открытия окна редактирования
-const profileName = document.querySelector('.profile__title'); //Имя на странице в секции профиль
-const profileJob = document.querySelector('.profile__subtitle'); //Должность на странице в секции профиль
-const profileForm = modalWindowProfile.querySelector('.popup__form');  //Форма профиля
-const nameInput = profileForm.elements.name; //Поле ввода имени
-const jobInput = profileForm.elements.job; //Поле ввода должности
-const profileCloseButton = modalWindowProfile.querySelector('.popup__button-close'); //Кнопка выхода из окна редактирования
-const cardsButton = document.querySelector('.profile__add-button'); //Кнопка открытия окна добавления карточек
-const cardsCloseButton = modalWindowCard.querySelector('.popup__button-close'); //Кнопка выхода из окна добавления карточек
-const cardsForm = modalWindowCard.querySelector('.popup__form'); //Форма добавления карточек
-const submitCardButton = cardsForm.querySelector('.popup__button-save');
-const titleInput = cardsForm.elements.title; //Поле ввода названия
-const urlInput = cardsForm.elements.url; //Поле ввода ссылки на картинку
-const elements = '.elements';  //Котнейнер с карточками
-const closeImageButton = modalWindowImage.querySelector('.popup__button-close');
 
 const cardsList = new Section({
   items: initialCards,
@@ -67,37 +60,33 @@ const profileEdit = new PopupWithForm(modalWindowProfile, {
 const user = new UserInfo({profileName: profileName, profileJob: profileJob});
 profileEdit.setEventListeners();
 
-
-
-
-function addValidation(formConfig){
-  const formList = Array.from(document.querySelectorAll(formConfig.formSelector));  
-  formList.forEach((formElement) => {
-      const validation = new FormValidator(formConfig, formElement);
-      validation.enableValidation();
-  });
-}
-
-addValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button-save',
-  inactiveButtonClass: 'popup__button-save_inactive',
-  inputErrorClass: 'popup__input_type_error'
-});
-
-
-
-
 profileButton.addEventListener('click', function(){
   profileEdit.open();
   const values = user.getUserInfo();
   nameInput.value = values.name;
   jobInput.value = values.job;
-})
+  const profileValidation = new FormValidator(
+    {
+      formSelector: '.popup__form',
+      inputSelector: '.popup__input',
+      submitButtonSelector: '.popup__button-save',
+      inactiveButtonClass: 'popup__button-save_inactive',
+      inputErrorClass: 'popup__input_type_error'
+    }, formProfile);
+    profileValidation.enableValidation();
+});
 
 cardsButton.addEventListener('click', function (){
   cardsEdit.open();
+  const cardValidation = new FormValidator(
+    {
+      formSelector: '.popup__form',
+      inputSelector: '.popup__input',
+      submitButtonSelector: '.popup__button-save',
+      inactiveButtonClass: 'popup__button-save_inactive',
+      inputErrorClass: 'popup__input_type_error'
+    }, formCard);
+    cardValidation.enableValidation();
 });  
 
 
--- utils/constants.js
+++ utils/constants.js
@@ -24,3 +24,16 @@ export const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
+export const modalWindowProfile = document.querySelector('#popup-profile'); //Окно с popup редактирования профиля
+export const modalWindowCard = document.querySelector('#popup-place'); //Окно с popup редактирования места
+export const modalWindowImage = document.querySelector('#popup-card'); //Окно с popup изображением
+export const profileButton = document.querySelector('.profile__edit-button'); //Кнопка открытия окна редактирования
+export const profileName = document.querySelector('.profile__title'); //Имя на странице в секции профиль
+export const profileJob = document.querySelector('.profile__subtitle'); //Должность на странице в секции профиль
+export const formProfile = document.forms.profile; //форма редактирования профиля
+export const nameInput = formProfile.elements.name; //Поле ввода имени
+export const jobInput = formProfile.elements.job; //Поле ввода должности
+export const cardsButton = document.querySelector('.profile__add-button'); //Кнопка открытия окна добавления карточек
+export const elements = '.elements';  //Котнейнер с карточками
+export const formCard = document.forms.place; //форма добавления карточки
