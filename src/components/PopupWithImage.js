0afa29203f8fe36351ee9334ce02b2d0104e4394
Ïgor Zakharov <Igor1994294@yandex.ru>
2020-10-04 19:30:02 +0300

    hotfix: переместить объект из конструктора в метод open

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,19 +1,18 @@
 import Popup from './Popup.js';
 export default class PopupWithImage extends Popup{
-  constructor(popupSelector, {src, alt}){
+  constructor(popupSelector){
     super(popupSelector);
-    this._src = src;
-    this._alt = alt;
     this._popupPic = popupSelector.querySelector('.popup__pic');
     this._popupSubtitle = popupSelector.querySelector('.popup__subtitle');
   }
   
 
-  open(){
-    this._popupSelector.classList.add('popup_opened');
+  open({src, alt}){
+    super.open();
+    this._src = src;
+    this._alt = alt;
     this._popupPic.src = this._src;
     this._popupPic.alt = this._alt;
     this._popupSubtitle.textContent = this._alt;
-    document.addEventListener('keydown', this._handleEscClose);
   };
 }
