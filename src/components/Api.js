80fdc92dde0c9eb261d1b2ec7a4ddfca270a6ebd
Ïgor Zakharov <Igor1994294@yandex.ru>
2020-10-13 16:03:09 +0300

    feat: добавить возможность редактированть аватар

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,29 +3,49 @@ export default class Api{
     this._baseUrl = apiOptions.baseUrl;
     this._headers = apiOptions.headers;
   }
+  
+  _checkRes(res){
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
 
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers
     })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Ошибка: ${res.status}`);
-      })
+      .then((res) => this._checkRes(res))
   } 
 
   getMyInfo(){
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers
     })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Ошибка: ${res.status}`);
-      }) 
+      .then((res) => this._checkRes(res)) 
   } 
+
+  patchMyInfo(name, about){
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: about
+      })
+    })
+      .then((res) => this._checkRes(res))
+  }
+
+  patchMyAvatar(avatar){
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatar
+      })
+    })
+      .then((res) => this._checkRes(res))
+  }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,21 @@ import {initialCards, apiOptions} from '../utils/constants.js';
 const modalWindowProfile = document.querySelector('#popup-profile'); //Окно с popup редактирования профиля
 const modalWindowCard = document.querySelector('#popup-place'); //Окно с popup редактирования места
 const modalWindowImage = document.querySelector('#popup-card'); //Окно с popup изображением
+const modalWindowAvatar = document.querySelector('#popup-avatar'); //Окно с редактированием аватара
 const profileButton = document.querySelector('.profile__edit-button'); //Кнопка открытия окна редактирования
 const profileName = document.querySelector('.profile__title'); //Имя на странице в секции профиль
 const profileJob = document.querySelector('.profile__subtitle'); //Должность на странице в секции профиль
-const profileAvatar = document.querySelector('.profile__image');
+const profileAvatar = document.querySelector('.profile__image');  //Аватар
 const formProfile = document.forms.profile; //форма редактирования профиля
 const nameInput = formProfile.elements.name; //Поле ввода имени
 const jobInput = formProfile.elements.job; //Поле ввода должности
 const cardsButton = document.querySelector('.profile__add-button'); //Кнопка открытия окна добавления карточек
 const elements = '.elements';  //Котнейнер с карточками
 const formCard = document.forms.place; //форма добавления карточки
-const cardSubmitButton = formCard.querySelector('.popup__button-save');
+const cardSubmitButton = formCard.querySelector('.popup__button-save');  //Кнопка создания карточки
+const avatarButton = document.querySelector('.profile__avatar-edit'); //Кнопка редактирования аватара
+const formAvatar = document.forms.avatar; //Форма обновления аватара
+const avatarSubmitButton = formAvatar.querySelector('.popup__button-save'); //Кнопка редактирования аватара
 const formData = 
 {
   formSelector: '.popup__form',
@@ -43,33 +47,38 @@ myInfo.then((data)=>{
 })
 .catch((err) =>{
   console.log(err);
-})
+});
 
-    
+const cardsLoader = api.getInitialCards();
+cardsLoader.then((data)=>{
+  const initCard = data.map(function (item){
+    return {name: item.name,
+            link: item.link};
+  });
+  const createCard = (item) =>{
+    const card = new Card({link: item.link, name: item.name}, '#element', {handleCardClick: () => {
+      popupCard.open({src: item.link, alt: item.name});
+    }
+    });
+    const cardElement = card.generateCard();
+    cardsList.addItem(cardElement);
+  }
+  const cardsList = new Section({
+    items: initCard,
+    renderer: (item) => {
+      createCard({name: item.name, link: item.link});
+      },
+    },
+    elements
+  );
+  cardsList.renderItems();
+});
+
+
 
 
 const popupCard = new PopupWithImage(modalWindowImage);
 popupCard.setEventListeners();
-
-// Создание карточки 
-const createCard = (item) =>{
-  const card = new Card({link: item.link, name: item.name}, '#element', {handleCardClick: () => {
-    popupCard.open({src: item.link, alt: item.name});
-  }
-  });
-  const cardElement = card.generateCard();
-  cardsList.addItem(cardElement);
-}
-
-//Добавление 6 начальных карточек
-const cardsList = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    createCard({name: item.name, link: item.link});
-    },
-  },
-  elements
-);
 
 //Добавление карточки через форму
 const cardsEdit = new PopupWithForm(modalWindowCard, {
@@ -78,12 +87,19 @@ const cardsEdit = new PopupWithForm(modalWindowCard, {
   }
 });
 cardsEdit.setEventListeners();
-cardsList.renderItems();
 
+const avatarEdit = new PopupWithForm(modalWindowAvatar, {
+  formSubmitHandler: (item) => {
+    api.patchMyAvatar(item.url);
+    profileAvatar.src = item.url;
+  }
+});
+avatarEdit.setEventListeners();
 
 const profileEdit = new PopupWithForm(modalWindowProfile, {
   formSubmitHandler: (item) => {
     user.setUserInfo({nameInput: item.name, jobInput: item.job});
+    api.patchMyInfo(item.name, item.job)
   }
 });
 const user = new UserInfo({profileName: profileName, profileJob: profileJob});
@@ -96,6 +112,10 @@ placeValidation.enableValidation();
 const profileValidation = new FormValidator(formData, formProfile);
 profileValidation.enableValidation();
 
+const avatarValidation = new FormValidator(formData, formAvatar);
+avatarValidation.enableValidation();
+
+
 
 profileButton.addEventListener('click', function(){
   profileEdit.open();
@@ -107,7 +127,13 @@ profileButton.addEventListener('click', function(){
 cardsButton.addEventListener('click', function (){
   placeValidation.disableButton(cardSubmitButton);
   cardsEdit.open();
-});  
+});
+
+avatarButton.addEventListener('click', function(){
+  avatarEdit.open();
+  avatarValidation.disableButton(avatarSubmitButton);
+})
+
 
 
 
